3b41234227d2917399e023bdf911a56c61ef9613
chrissi21509 <chrisharrisonn123@gmail.com>
2025-04-15 14:50:09 -0700

    Update weather.ts

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -1,3 +1,5 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', 'https://blockify.icu')
